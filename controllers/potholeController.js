057661da550d78bc985f050e9ef72e642754164b
ΛTᏥΛᏒᐯΛ <115201572+atharv0905@users.noreply.github.com>
2024-03-03 23:16:33 +0530

    Added pothole controller functions
    getAllPotholeData
    getLatLngOfAllPothole
    getImageOfSpecificPothole
    deletePotholeById

--- controllers/potholeController.js
+++ controllers/potholeController.js
@@ -1,5 +1,6 @@
 const Pothole = require('../models/potholeSchema');
 const path = require('path');
+const fs = require('fs').promises;
 
 // ----------------------------------------------------------------------------------------------------------------------------------
 // add new pothole
@@ -26,7 +27,69 @@ const addNewPothole = async (req, res) => {
 // add new pothole
 // ----------------------------------------------------------------------------------------------------------------------------------
 
+// ----------------------------------------------------------------------------------------------------------------------------------
+// get all pothole data
+// ----------------------------------------------------------------------------------------------------------------------------------
+const getAllPotholeData = async (req, res) => {
+    try {
+        const potholes = await Pothole.find();
+        res.status(200).json(potholes);
+    } catch (error) {
+        res.status(500).send(`Error fetching locations: ${error.message}`);
+    }
+};
+
+const getLatLngOfAllPothole = async (req, res) => {
+    try {
+        const potholes = await Pothole.find({}, {latitude: 1, longitude: 1});
+        res.status(200).json(potholes);
+    } catch (error) {
+        res.status(500).send(`Error fetching locations: ${error.message}`);
+    }
+}
+
+const getImageOfSpecificPothole = async (req, res) => {
+    try {
+        const pothole = await Pothole.findById(req.query.id);
+        // res.status(200).send(pothole.imagePath);
+        res.status(200).sendFile(path.resolve(pothole.imagePath));
+    } catch (error) {
+        res.status(500).send(`Error fetching locations: ${error.message}`);
+    }
+};
+// ----------------------------------------------------------------------------------------------------------------------------------
+// get all pothole data
+// ----------------------------------------------------------------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------------------------------------------------------------
+// resolve pothole issue
+const deletePotholeById = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const pothole = await Pothole.findById(req.params.id);
+        if (!pothole) {
+            return res.status(404).send('Pothole not found');
+        }
+        
+        // Delete the image file
+        await fs.unlink(path.resolve(pothole.imagePath));
+        
+        // Delete the pothole record from the database
+        await Pothole.findByIdAndDelete(req.params.id);
+
+        res.status(200).send('Pothole and corresponding image deleted successfully');
+    } catch (error) {
+        res.status(500).send(`Error deleting pothole: ${error.message}`);
+    }
+};
+// resolve pothole issue
+// ----------------------------------------------------------------------------------------------------------------------------------
+
 
 module.exports = {
-    addNewPothole
+    addNewPothole,
+    getAllPotholeData,
+    getLatLngOfAllPothole,
+    getImageOfSpecificPothole,
+    deletePotholeById
 };
